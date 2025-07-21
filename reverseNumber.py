9541c19748e0293e52e3e9a4c59761f19b66db08
KMONISH <monishkb1999@gmail.com>
2025-07-21 20:36:37 +0530

    python

---
+++ reverseNumber.py
@@ -0,0 +1,14 @@
+org_number = 1438
+reverseNum = 0
+while org_number>0:
+    rem = org_number % 10
+    org_number //= 10
+    reverseNum = reverseNum * 10 + rem
+print("Reversed Number is:",reverseNum)
+
+# Reversed Number is: 341
+
+
+
+
+
